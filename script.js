c4e83252dc9c1a752b36a7f388895bfadeb85026
SIVAJB <121388913+sivajeyabalan@users.noreply.github.com>
2024-06-15 20:09:28 +0530

    added a range button

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const gridsize = 600;
-let rows = 16;
-let col = 16;
-
+let  squarePerSide = 16 ;
 const sketcharea = document.getElementById("sketch-area");
-sketcharea.style.width = `${gridsize}px`;
-sketcharea.style.height = `${gridsize}px`;
+const sliderContainer = document.getElementById("slider-container");
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
 
+sliderValue.textContent =  `${slider.value} x ${slider.value} (Resolution)` ;
+sketcharea.style.width = sketcharea.style.height = `${gridsize}px`;
+
+
+
+function setbackColor()
+{
+  this.style.backgroundColor = "green" ;
+}
 function createGrid() {
-  for (let i = 0; i < (col * rows); i++) {
+  const numOfSquares = (squarePerSide * squarePerSide);
+  const widthOrHeight = `${(gridsize / squarePerSide) - 2}px`;
+  for (let i = 0; i < numOfSquares; i++) {
     const gridcell = document.createElement("div");
-    gridcell.style.width = `${gridsize / col}px`;
-    gridcell.style.height = `${gridsize / rows}px`;
+    gridcell.style.width = gridcell.style.height = widthOrHeight ;
     gridcell.classList.add("cell");
 
     sketcharea.appendChild(gridcell);
+
+    gridcell.addEventListener("mouseover",setbackColor);
+  }
+}
+
+
+
+function removeCells(){
+  while(sketcharea.firstChild){
+    sketcharea.replaceChild(sketcharea.firstChild);
   }
 }
 
